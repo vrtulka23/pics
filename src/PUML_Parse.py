f216b6616a9d89a85d72e593639b24206098bb96
Ondrej Pego Jaura <ondrej.jaura@marvelfusion.com>
2022-12-02 21:15:19 +0100

    introduced PUML_Unit

--- src/PUML_Parse.py
+++ src/PUML_Parse.py
@@ -1,82 +1,104 @@
 from pydantic import BaseModel
+from typing import List
 import numpy as np
 import re
+
+class PUML_Unit(BaseModel):
+    num: float
+    base: List[int]
+    definition: str = None
+    name: str = None
+
+    def __init__(self,num,base,definition=None,name=None,**kwargs):
+        kwargs['num'] = num
+        kwargs['base'] = base
+        kwargs['definition'] = definition
+        kwargs['name'] = name
+        super().__init__(**kwargs)
 
 class PUML_Parse:
 
     base = {
         # physical units
-        'm':    (1.0, [1,0,0,0,0,0,0,0,0]),  # 1
-        'g':    (1.0, [0,1,0,0,0,0,0,0,0]),  # 2
-        's':    (1.0, [0,0,1,0,0,0,0,0,0]),  # 3
-        'K':    (1.0, [0,0,0,1,0,0,0,0,0]),  # 4
-        'C':    (1.0, [0,0,0,0,1,0,0,0,0]),  # 5
-        'cd':   (1.0, [0,0,0,0,0,1,0,0,0]),  # 6
+        'm':    PUML_Unit(1.0, [1,0,0,0,0,0,0,0,0]),  # 1
+        'g':    PUML_Unit(1.0, [0,1,0,0,0,0,0,0,0]),  # 2
+        's':    PUML_Unit(1.0, [0,0,1,0,0,0,0,0,0]),  # 3
+        'K':    PUML_Unit(1.0, [0,0,0,1,0,0,0,0,0]),  # 4
+        'C':    PUML_Unit(1.0, [0,0,0,0,1,0,0,0,0]),  # 5
+        'cd':   PUML_Unit(1.0, [0,0,0,0,0,1,0,0,0]),  # 6
         # numerical units
-        'mol':  (1.0, [0,0,0,0,0,0,1,0,0]),  # 7
-        'rad':  (1.0, [0,0,0,0,0,0,0,1,0]),  # 8
-        '10*':  (1.0, [0,0,0,0,0,0,0,0,1]),  # 9
+        'mol':  PUML_Unit(1.0, [0,0,0,0,0,0,1,0,0]),  # 7
+        'rad':  PUML_Unit(1.0, [0,0,0,0,0,0,0,1,0]),  # 8
+        '10*':  PUML_Unit(1.0, [0,0,0,0,0,0,0,0,1]),  # 9
     }
-    derivates = { # m,g,s,K,C,cd,rad,10
+    derivates = { # m,g,s,K,C,cd,mol,rad,10
         # SI units
-        'sr':    (1.0,  [ 0, 0, 0, 0, 0, 0, 0, 2, 0]), 	# steradian (rad2)
-        'Hz':    (1.0,  [ 0, 0,-1, 0, 0, 0, 0, 0, 0]), 	# hertz (s-1)
-        'N':     (1.0,  [ 1, 1,-2, 0, 0, 0, 0, 0, 3]), 	# newton (kg.m/s2)
-        'Pa':    (1.0,  [-1, 1,-2, 0, 0, 0, 0, 0, 3]), 	# pascal (N/m2)
-        'J':     (1.0,  [ 2, 1,-2, 0, 0, 0, 0, 0, 3]), 	# joule (N.m)
-        'W':     (1.0,  [ 2, 1,-3, 0, 0, 0, 0, 0, 3]), 	# watt (J/s)
-        'A':     (1.0,  [ 0, 0,-1, 0, 1, 0, 0, 0, 0]), 	# ampere (C/s)
-        'V':     (1.0,  [ 2, 1,-2, 0,-1, 0, 0, 0, 3]), 	# volt (J/C)
-        'F':     (1.0,  [-2,-1, 2, 0, 2, 0, 0, 0,-3]), 	# farad (C/V)
-        'Ohm':   (1.0,  [ 2, 1,-1, 0,-2, 0, 0, 0, 3]), 	# ohm (V/A)
-        'S':     (1.0,  [-2,-1, 1, 0, 2, 0, 0, 0,-3]), 	# siemens (Ohm-1)
-        'Wb':    (1.0,  [ 2, 1,-1, 0,-1, 0, 0, 0, 3]), 	# weber (V.s)
-        'T':     (1.0,  [ 0, 1,-1, 0,-1, 0, 0, 0, 3]), 	# tesla (Wb/m2)
-        'H':     (1.0,  [ 2, 1, 0, 0,-2, 0, 0, 0, 3]),  # henry (Wb/A)
-        'lm':    (1.0,  [ 0, 0, 0, 0, 0, 1, 0, 2, 0]), 	# lumen (cd.sr)
-        'lx':    (1.0,  [-2, 0, 0, 0, 0, 1, 0, 2, 0]), 	# lux (lm/m2)
-        'Bq':    (1.0,  [ 0, 0,-1, 0, 0, 0, 0, 0, 0]), 	# becquerel (s-1)
-        'Gy':    (1.0,  [ 2, 0,-2, 0, 0, 0, 0, 0, 0]), 	# gray (J/kg)
-        'Sv':    (1.0,  [ 2, 0,-2, 0, 0, 0, 0, 0, 0]), 	# sivert (J/kg)
+        'sr':    PUML_Unit(1.0,  [ 0, 0, 0, 0, 0, 0, 0, 2, 0], 'rad2',    'steradian'),
+        'Hz':    PUML_Unit(1.0,  [ 0, 0,-1, 0, 0, 0, 0, 0, 0], 's-1',     'hertz'    ),
+        'N':     PUML_Unit(1.0,  [ 1, 1,-2, 0, 0, 0, 0, 0, 3], 'kg.m/s2', 'newton'   ),
+        'Pa':    PUML_Unit(1.0,  [-1, 1,-2, 0, 0, 0, 0, 0, 3], 'N/m2',    'pascal'   ),
+        'J':     PUML_Unit(1.0,  [ 2, 1,-2, 0, 0, 0, 0, 0, 3], 'N.m',     'joule'    ),
+        'W':     PUML_Unit(1.0,  [ 2, 1,-3, 0, 0, 0, 0, 0, 3], 'J/s',     'watt'     ),
+        'A':     PUML_Unit(1.0,  [ 0, 0,-1, 0, 1, 0, 0, 0, 0], 'C/s',     'ampere'   ),
+        'V':     PUML_Unit(1.0,  [ 2, 1,-2, 0,-1, 0, 0, 0, 3], 'J/C',     'volt'     ),
+        'F':     PUML_Unit(1.0,  [-2,-1, 2, 0, 2, 0, 0, 0,-3], 'C/V',     'farad'    ),
+        'Ohm':   PUML_Unit(1.0,  [ 2, 1,-1, 0,-2, 0, 0, 0, 3], 'V/A',     'ohm'      ),
+        'S':     PUML_Unit(1.0,  [-2,-1, 1, 0, 2, 0, 0, 0,-3], 'Ohm-1',   'siemens'  ),
+        'Wb':    PUML_Unit(1.0,  [ 2, 1,-1, 0,-1, 0, 0, 0, 3], 'V.s',     'weber'    ),
+        'T':     PUML_Unit(1.0,  [ 0, 1,-1, 0,-1, 0, 0, 0, 3], 'Wb/m2',   'tesla'    ),
+        'H':     PUML_Unit(1.0,  [ 2, 1, 0, 0,-2, 0, 0, 0, 3], 'Wb/A',    'henry'    ),
+        'lm':    PUML_Unit(1.0,  [ 0, 0, 0, 0, 0, 1, 0, 2, 0], 'cd.sr',   'lumen'    ),
+        'lx':    PUML_Unit(1.0,  [-2, 0, 0, 0, 0, 1, 0, 2, 0], 'lm/m2',   'lux'      ),
+        'Bq':    PUML_Unit(1.0,  [ 0, 0,-1, 0, 0, 0, 0, 0, 0], 's-1',     'becquerel'),
+        'Gy':    PUML_Unit(1.0,  [ 2, 0,-2, 0, 0, 0, 0, 0, 0], 'J/kg',    'gray'     ),
+        'Sv':    PUML_Unit(1.0,  [ 2, 0,-2, 0, 0, 0, 0, 0, 0], 'J/kg',    'sivert'   ),
         # CGS units                          
-        'dyn':   (1.0,  [ 1, 1,-2, 0, 0, 0, 0, 0,-2]),     # dyne (g.cm/s2)
-        'erg':   (1.0,  [ 2, 1,-2, 0, 0, 0, 0, 0,-4]),     # erg (dyn.cm)
-        'G':     (1.0,  [ 0, 1,-1, 0,-1, 0, 0, 0,-1]),     # Gauss (10*-4.T)
+        'dyn':   PUML_Unit(1.0,  [ 1, 1,-2, 0, 0, 0, 0, 0,-2], 'g.cm/s2', 'dyne'     ),
+        'erg':   PUML_Unit(1.0,  [ 2, 1,-2, 0, 0, 0, 0, 0,-4], 'dyn.cm',  'erg'      ),
+        'G':     PUML_Unit(1.0,  [ 0, 1,-1, 0,-1, 0, 0, 0,-1], '10*-4.T', 'Gauss'    ),
+        # other derived units
+        'deg':   PUML_Unit(57.29577951308232, [ 0, 0, 0, 0, 0,0,0,1,  0],
+                           'rad.180/[pi]', 'degree'       ),
+        'eV':    PUML_Unit(1.60217733,        [ 2, 1,-2, 0, 0,0,0,0,-16],
+                           '[e].V',        'electronvolt'),
+        # natural units                                           
+        '[e]':   PUML_Unit(1.60217733,        [ 0, 0, 0, 0, 1, 0, 0, 0,-19],
+                           None, 'elementary charge'),
+        # dimensionless units
+        '[pi]':    PUML_Unit(np.pi,      [0,0,0,0,0,0,0,0, 0], None, 'pi'),
+        '[euler]': PUML_Unit(np.e,       [0,0,0,0,0,0,0,0, 0], None, "euler's number"),
+        '[N_A]':   PUML_Unit(6.0221367,  [0,0,0,0,0,0,0,0,23], None, "avogadro's number"),
+        #'%':       PUML_Unit(1.0,        [0,0,0,0,0,0,0,0,-2]# percent
     }
     prefixes = {  
-        'Y':    (1.0, [0,0,0,0,0,0,0,0, 24]),  # yotta
-        'Z':    (1.0, [0,0,0,0,0,0,0,0, 21]),  # zetta
-        'E':    (1.0, [0,0,0,0,0,0,0,0, 18]),  # exa
-        'P':    (1.0, [0,0,0,0,0,0,0,0, 15]),  # peta
-        'T':    (1.0, [0,0,0,0,0,0,0,0, 12]),  # tera
-        'G':    (1.0, [0,0,0,0,0,0,0,0,  9]),  # giga
-        'M':    (1.0, [0,0,0,0,0,0,0,0,  6]),  # mega
-        'k':    (1.0, [0,0,0,0,0,0,0,0,  3]),  # kilo
-        'h':    (1.0, [0,0,0,0,0,0,0,0,  2]),  # hectoh
-        'da':   (1.0, [0,0,0,0,0,0,0,0,  1]),  # deka
-        'd':    (1.0, [0,0,0,0,0,0,0,0, -1]),  # deci
-        'c':    (1.0, [0,0,0,0,0,0,0,0, -2]),  # centi
-        'm':    (1.0, [0,0,0,0,0,0,0,0, -3]),  # milli
-        'u':    (1.0, [0,0,0,0,0,0,0,0, -6]),  # micro
-        'n':    (1.0, [0,0,0,0,0,0,0,0, -9]),  # nano
-        'p':    (1.0, [0,0,0,0,0,0,0,0,-12]),  # pico
-        'f':    (1.0, [0,0,0,0,0,0,0,0,-15]),  # femto
-        'a':    (1.0, [0,0,0,0,0,0,0,0,-18]),  # atto
-        'z':    (1.0, [0,0,0,0,0,0,0,0,-21]),  # zepto
-        'y':    (1.0, [0,0,0,0,0,0,0,0,-24]),  # yocto
-    }
-    numbers = {  
-        '[pi]':    (np.pi,     [0,0,0,0,0,0,0,0, 0]),  # pi
-        '[euler]': (np.e,      [0,0,0,0,0,0,0,0, 0]),  # euler's number
-        '[N_A]':   (6.0221367, [0,0,0,0,0,0,0,0,23]),  # avogadro's number
-        '%':       (1.0,       [0,0,0,0,0,0,0,0,-2]),  # percent
+        'Y':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0, 24]),  # yotta
+        'Z':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0, 21]),  # zetta
+        'E':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0, 18]),  # exa
+        'P':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0, 15]),  # peta
+        'T':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0, 12]),  # tera
+        'G':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0,  9]),  # giga
+        'M':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0,  6]),  # mega
+        'k':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0,  3]),  # kilo
+        'h':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0,  2]),  # hectoh
+        'da':   PUML_Unit(1.0, [0,0,0,0,0,0,0,0,  1]),  # deka
+        'd':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0, -1]),  # deci
+        'c':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0, -2]),  # centi
+        'm':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0, -3]),  # milli
+        'u':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0, -6]),  # micro
+        'n':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0, -9]),  # nano
+        'p':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0,-12]),  # pico
+        'f':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0,-15]),  # femto
+        'a':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0,-18]),  # atto
+        'z':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0,-21]),  # zepto
+        'y':    PUML_Unit(1.0, [0,0,0,0,0,0,0,0,-24]),  # yocto
     }
     other = {
-        'Cel':   (1.0,  [0, 0, 0, 1, 0, 0, 0, 0, 0]), 	# degree (cel(1 K))
+        'Cel':   PUML_Unit(1.0,  [0, 0, 0, 1, 0, 0, 0, 0, 0]), 	# degree (cel(1 K))
     }
 
     def __init__(self):
         self.nbase = len(self.base)
+        self.npbase = self.nbase-3
         self.units = self.base | self.derivates
 
     def __enter__(self):
@@ -86,26 +108,26 @@ class PUML_Parse:
         pass
     
     def equal(self, unit1, unit2):
-        if unit1[0]!=unit2[0]:
+        if unit1.num!=unit2.num:
             return False
-        if unit1[1]!=unit2[1]:
+        if unit1.base!=unit2.base:
             return False
         return True
     
     def multiply(self, unit1, unit2):
-        num = unit1[0]*unit2[0]
-        base = [unit1[1][i]+unit2[1][i] for i in range(self.nbase)]
-        return (num, base)
+        num = unit1.num*unit2.num
+        base = [unit1.base[i]+unit2.base[i] for i in range(self.nbase)]
+        return PUML_Unit(num, base)
 
     def divide(self, unit1, unit2):
-        num = unit1[0]/unit2[0]
-        base = [unit1[1][i]-unit2[1][i] for i in range(self.nbase)]
-        return (num, base)
+        num = unit1.num/unit2.num
+        base = [unit1.base[i]-unit2.base[i] for i in range(self.nbase)]
+        return PUML_Unit(num, base)
 
     def power(self, unit, power):
-        num = unit[0]**power
-        base = [unit[1][i]*power for i in range(self.nbase)]
-        return (num, base)
+        num = unit.num**power
+        base = [unit.base[i]*power for i in range(self.nbase)]
+        return PUML_Unit(num, base)
 
     def unit(self, unit):
         unit_bak = unit
@@ -132,12 +154,17 @@ class PUML_Parse:
             symbol, unit = unit[-1], unit[:-1]
             if symbol==' ':
                 break
+        # return dimensionless numbers
+        if exp and base=='' and prefix=='':
+            return PUML_Unit(int(exp), [0]*self.nbase)
+        # apply prefix
         if prefix:
             if prefix not in self.prefixes.keys():
                 raise Exception(f"Unit prefix '{prefix}' is not available in: {unit_bak}")
             unit = self.multiply(self.prefixes[prefix],self.units[base])
         else:
             unit = self.units[base]
+        # apply exponent
         if exp:
             unit = self.power(unit,int(exp))
         #print("%-06s"%unit_bak, "%-03s"%prefix, "%-03s"%base, "%03s"%exp, unit)
@@ -158,6 +185,13 @@ class PUML_Parse:
                     self.expression(part2, expr_bak)
                 )
             elif symbol=='/':
+                if '/' in part2:
+                    # If there are multiple divisions
+                    # we need to start from the last
+                    parts = part2.split('/')
+                    part2 = parts.pop()
+                    parts.insert(0,part1)
+                    part1 = '/'.join(parts)
                 return self.divide(
                     self.expression(part1, expr_bak),
                     self.expression(part2, expr_bak)
@@ -179,4 +213,13 @@ class PUML_Parse:
                 part1 = part1 + symbol
                 symbol, part2 = part2[0], part2[1:]
         return self.unit(part1+symbol)
-    
+        
+    def convert(self, value, exp1, exp2):
+        unit1 = self.expression(exp1)
+        unit2 = self.expression(exp2)
+        factor = self.divide(unit1,unit2)
+        if factor.base[:self.npbase]!=[0]*self.npbase:
+            raise Exception(f"Units '{exp1}' and '{exp2}' cannot be converted")
+        value *=  factor.num
+        value *= 10**factor.base[self.base['10*'].base.index(1)]
+        return value
--- tests/test_units.py
+++ tests/test_units.py
@@ -4,37 +4,53 @@ from PUML_Parse import *
 import pytest
 import numpy as np
 
+def test_base():
+    with PUML_Parse() as p:
+        # Closure of base units
+        unit = PUML_Unit(1.0, [0 for i in range(p.nbase)])
+        for base in p.base.values():
+            unit = p.multiply(unit,base)
+        print('Base:',unit)
+        assert p.equal(unit, PUML_Unit(1.0, [1 for i in range(p.nbase)]))
+
 def test_operations():
     with PUML_Parse() as p:
-        unit1 = (2.0, [i for i in range(1,1+p.nbase)])
-        unit2 = (2.0, [i for i in range(2,2+p.nbase)])
+        # Multiplication
+        unit1 = PUML_Unit(2.0, [i for i in range(1,1+p.nbase)])
+        unit2 = PUML_Unit(2.0, [i for i in range(2,2+p.nbase)])
         unit3 = p.multiply(unit1, unit2)
-        unit4 = (unit1[0]*unit2[0], [unit1[1][i]+unit2[1][i] for i in range(p.nbase)])
+        unit4 = PUML_Unit(unit1[0]*unit2[0], [unit1[1][i]+unit2[1][i] for i in range(p.nbase)])
         print(' ',unit1,'\n*',unit2,'\n=',unit3,'\n')
         assert p.equal(unit3, unit4)
-
-        unit1 = (4.0, [i+i for i in range(1,1+p.nbase)])
-        unit2 = (2.0, [i for i in range(1,1+p.nbase)])
+        # Division
+        unit1 = PUML_Unit(4.0, [i+i for i in range(1,1+p.nbase)])
+        unit2 = PUML_Unit(2.0, [i for i in range(1,1+p.nbase)])
         unit3 = p.divide(unit1, unit2)
-        unit4 = (unit1[0]/unit2[0], [unit1[1][i]-unit2[1][i] for i in range(p.nbase)])
+        unit4 = PUML_Unit(unit1[0]/unit2[0], [unit1[1][i]-unit2[1][i] for i in range(p.nbase)])
         print(' ',unit1,'\n/',unit2,'\n=',unit3,'\n')
         assert p.equal(unit3, unit4)
-
-        unit1 = (2.0, [i for i in range(1,1+p.nbase)])
+        # Power
+        unit1 = PUML_Unit(2.0, [i for i in range(1,1+p.nbase)])
         power = 3
         unit2 = p.power(unit1, power)
-        unit3 = (unit1[0]**power, [unit1[1][i]*power for i in range(p.nbase)])
+        unit3 = PUML_Unit(unit1[0]**power, [unit1[1][i]*power for i in range(p.nbase)])
         print(' ',unit1,'\n^',power,'\n=',unit3)
         assert p.equal(unit2, unit3)
         
 def test_units():
     with PUML_Parse() as p:
         units = {
-            'm':     p.units['m'],
-            'm-2':   p.power(p.units['m'],-2),
-            'mm':    p.multiply(p.prefixes['m'], p.units['m']),
-            'km2':   p.power(p.multiply(p.prefixes['k'], p.units['m']), 2),
-            'uOhm3': p.power(p.multiply(p.prefixes['u'], p.units['Ohm']), 3),
+            'm':     p.units['m'],                               # just a unit                      
+            'm-2':   p.power(p.units['m'],-2),                   # exponents
+            'mm':    p.multiply(p.prefixes['m'], p.units['m']),  # prefixes
+            'km2':   p.power(                                    # all together
+                p.multiply(p.prefixes['k'], p.units['m']),
+                2
+            ),
+            'uOhm3': p.power(                                    # units with long names
+                p.multiply(p.prefixes['u'],p.units['Ohm']),
+                3
+            ),
         }
         for name, unit2 in units.items():
             unit1 = p.unit(name)
@@ -47,46 +63,31 @@ def test_expressions():
         newton = p.multiply(newton,p.base['m'])
         newton = p.divide(newton,p.power(p.base['s'],2))
         units = {
-            'N': 'kg.m/s2',
-            'Pa': 'kg/(s2.m)',
-            'J': '(kg.m2)/s2',
-            'W': 'kg.(m2/s3)',
-            'V': 'kg.(m2/(s2.C))',
+            'N':   'kg.m/s2',        # basic operations
+            'Pa':  'kg/(s2.m)',      # parentheses in denominator
+            'J':   '(kg.m2)/s2',     # parentheses in nominator
+            'W':   'kg.(m2/s3)',     # fraction in parentheses
+            'A':   'C.s-1',          # negative exponents
+            'V':   'kg.(m2/(s2.C))', # nested parentheses
+            'Ohm': '((kg.m2)/s)/C2', # multiple fractions with parentheses
+            'S':   's.C2/kg/m2',     # multiple fractions without parentheses
+            'deg': 'rad.180/[pi]',   # numbers and constants
         }
         for name, expr in units.items():
             unit1 = p.units[name]
             unit2 = p.expression(expr)
             print("%-03s %-15s"%(name,expr), unit1)
-            assert p.equal(unit1, unit2)
-        
-def test_derived_si():
-    units = {
-        'sr':  'rad2',
-        'Hz':  's-1',
-        'N':   'kg.m/s2',
-        'Pa':  'N/m2',
-        'J':   'N.m',
-        'W':   'J/s',
-        'A':   'C/s',
-        'V':   'J/C',
-        'F':   'C/V',
-        'Ohm': 'V/A',
-        'S':   'Ohm-1',
-        'Wb':  'V.s',
-        'T':   'Wb/m2',
-        'H':   'Wb/A',
-        'lm':  'cd.sr',
-        'lx':  'lm/m2',
-        'Bq':  's-1',
-        'Gy':  'J/kg',
-        'Sv':  'J/kg',
-    }
+            assert p.equal(unit1, unit2)        
+
+def test_derivates():
+    # Check if derived units are correct
     with PUML_Parse() as p:
-        for name, expr in units.items():
-            unit1 = p.expression(expr)
-            unit2 = p.units[name]
-            print("%-03s"%name, unit1)
-            assert p.equal(unit1, unit2)            
+        for sign, unit in p.derivates.items():
+            if not unit.definition:
+                continue
+            expr = p.expression(unit.definition)
+            print("%-13s"%unit.name, "%-3s"%sign, "%-12s"%unit.definition, expr)
+            assert p.equal(expr, unit)            
             
 def test_derived_cgs():
     units = {
@@ -101,6 +102,20 @@ def test_derived_cgs():
             print("%-03s"%name, unit1)
             assert p.equal(unit1, unit2)            
 
+def test_convert():
+    examples = [
+        (1, 'm',   1e-3,              'km'),
+        (1, 'kJ',  1e3,               'J'),
+        (1, 'eV',  1.60217733e-4,     'fJ'),
+        (1, 'erg', 624.1506363094028, 'GeV'),
+    ]
+    with PUML_Parse() as p:
+        for e in examples:
+            print(f"{e[0]} {e[1]:4s} = {e[2]:.4f} {e[3]}")
+            assert p.convert(e[0], e[1], e[3]) == e[2]
+
+        print(p.expression('rad.180/[pi]'))
+            
 if __name__ == "__main__":
     # Specify wich test to run
     test = sys.argv[1] if len(sys.argv)>1 else True
